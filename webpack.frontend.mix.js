bb49945428f429ed09d7cf14843f3d27c24ace1c
Angus Simons <info@simonepozzobon.com>
2019-07-04 02:49:06 +0200

    Update librerie e aggiornamento admin

--- webpack.frontend.mix.js
+++ webpack.frontend.mix.js
@@ -1,5 +1,7 @@
 const mix = require('laravel-mix')
 require('laravel-mix-purgecss')
+require('laravel-mix-polyfill')
+
 // require('laravel-mix-bundle-analyzer')
 
 
@@ -12,6 +14,12 @@ mix
     .js('resources/js/app.js', 'public/js')
     // .extract(['vue', 'lodash', 'lottie-web', 'jquery', 'ScrollMagic', 'gsap'])
     .version()
+    .polyfill({
+        enabled: true,
+        useBuiltIns: 'usage',
+        targets: 'last 2 version, not dead',
+        debug: true
+    })
     // .bundleAnalyzer()
     .webpackConfig(webpack => {
         return {
